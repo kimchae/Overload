c7e84b4046c784d68adf5888ae0ab17497a7b7e2
Kaioru <keifuurawrs@gmail.com>
2017-04-12 17:48:06 +0800

    Import Lumen and Dingo starter

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Dingo\Api\Routing\Router;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->version();
+});
+
+$api = app(Router::class);
+$api->version('v1', function ($api) {
+    // Define your API routes here.
+});
